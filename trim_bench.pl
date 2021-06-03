8e6fdf716796207b685623051dfb63c8648cb971
Yves Orton <demerphq@gmail.com>
2021-06-03 08:53:22 +0200

    flexible bm

--- trim_bench.pl
+++ trim_bench.pl
@@ -3,12 +3,16 @@ use warnings;
 use Benchmark qw(cmpthese timethese :hireswallclock);
 use Test::More;
 use blib;
+use Data::Dumper;
 use DFA::Trim qw(trimmed);
+use Char::Replace qw(trim);
 my $base= 'naive';
 my @extra= qw(separate loop2 trimmed);
+my %extra= map { $_ => 1 } @extra;
+my $bm_time= $ENV{CHECK} ? 100000 : -1;
+
 my @keys= ($base,@extra);
-my $sp= $ENV{UNICODE} ? "\x{1680}" : " ";
-my $ns= $ENV{UNICODE} ? "\x{100}" : "x";
+
 $|++;
 printf "%6s %6s %1s %1s %4s %3s %3s|%10s|".("%10s %10s|" x @extra)."\n", 
     "blen","clen","S","U", "reps","ns","sp", $base."/s", map { $_."/s" , "as pct nv" } @extra;
@@ -52,38 +56,38 @@ foreach my $ns_len (1,2,5,10,25,100) {
                     my $string= ($sp x $sp_len) . ((($ns x $ns_len) . ($sp x $sp_len))x$segments);
                     my $utf8_string= $string;
                     utf8::encode($utf8_string);
-                    my ($naive,$separate,$loop2,$trimmed);
+                    my ($naive,$separate,$loop2,$trimmed,$trim);
                     #diag "timing $descr\n";
                     my $clen= length $string;
                     my $blen= length $utf8_string;
 
-                    my ($naive_sub,$separate_sub,$loop2_sub);
+                    my %sub;
                     if ($as_sub) {
-                        $naive_sub= sub { 
+                        $sub{naive}= sub {
                             $naive= naive($string); 
                             return; 
                         };
-                        $separate_sub= sub { 
+                        $sub{separate}= sub {
                             $separate= separate($string);
                             return;
                         };
-                        $loop2_sub= sub{ 
+                        $sub{loop2}= sub{
                             $loop2= loop2($string);
                             return;
                         };
                     } else {
-                        $naive_sub= sub { 
+                        $sub{naive}= sub {
                             $naive= $string; 
                             $naive=~s/\A\s+|\s+\z//g;  
                             return;
                         };
-                        $separate_sub= sub { 
+                        $sub{separate}= sub {
                             $separate= $string; 
                             $separate=~s/\A\s+//; 
                             $separate=~s/\s+\z//; 
                             return;
                         };
-                        $loop2_sub= sub { 
+                        $sub{loop2}= sub {
                             $loop2= $string; 
                             $loop2=~s/\A\s+//; 
                             1 while 
@@ -95,13 +99,20 @@ foreach my $ns_len (1,2,5,10,25,100) {
                             return;
                         };
                     }
-                       
-                    my $r= timethese -1, {
-                        naive => $naive_sub,
-                        separate => $separate_sub,
-                        loop2 => $loop2_sub,
-                        'trimmed' => sub { $trimmed= trimmed($string); return; },
-                    }, "none";
+                    $sub{trimmed}= sub {
+                        $trimmed= trimmed($string);
+                        return;
+                    };
+                    $sub{trim} = sub {
+                        $trim= Char::Replace::trim($string);
+                        return;
+                    };
+
+                    my %run;
+                    $run{naive}= $sub{naive};
+                    $run{$_}= $sub{$_} for @extra;
+
+                    my $r= timethese($bm_time, \%run, "none");
                     my @key= ($blen,$clen,$as_sub,$unicode,$segments,$ns_len,$sp_len);
                     my %rps;
                     my $max;
@@ -121,12 +132,17 @@ foreach my $ns_len (1,2,5,10,25,100) {
                             if $name ne "naive";
                     }
                     printf "%6d %6d %1d %1d %4d %3d %3d|%10s|". ("%10s %10s|" x @extra) . "\n", @key, @data;
-                    $ENV{CHECK} and ok(
-                        $naive &&
-                        $naive eq $separate && 
-                        $naive eq $loop2 &&
-                        $naive eq $trimmed, "all results the same - $descr"
-                    );
+                    if ($ENV{CHECK}) {
+                        ok($naive,"naive is true");
+                        ok($naive eq $separate,"naive and separate are the same")
+                            if $extra{separate};
+                        ok($naive eq $loop2,"naive and loop2 are the same")
+                            if $extra{loop2};
+                        ok($naive eq $trim,"naive and Char::Replace::trim are the same")
+                            if $extra{trim};
+                        ok($naive eq $trimmed,"naive and DFA::Trim::trimmed are the same")
+                            if $extra{trimmed};
+                    }
                 }
             }
         }
